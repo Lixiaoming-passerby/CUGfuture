8dcecd697a0affe46e58a9f40023a2f1a0566973
Lixiaoming-passerby <77234748+Lixiaoming-passerby@users.noreply.github.com>
2021-05-04 01:07:31 +0800

    Add files via upload
    --story=1006904 --user=ljm 校园地图 https://www.tapd.cn/64273778/s/1013943

---
+++ maps.js
@@ -0,0 +1,103 @@
+// pages/maps/maps.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    latitude: "",
+    longitude: "",
+    scale: 14,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    //获取当前的地理位置、速度
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        //赋值经纬度
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      }
+    })
+
+  },
+  
+  back:function(){
+    const mpCtx = wx.createMapContext("map");
+    mpCtx.moveToLocation();
+  },
+
+  update:function(){
+    var that = this
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        //赋值经纬度
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      }
+    })
+
+
+  },
+
+ 
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
